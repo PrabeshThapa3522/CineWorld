feb4158cfa9be6b77d8cdd03b913d3ab42615159
PrabeshThapa3522 <thapa.pranish7835@gmail.com>
2025-07-25 12:16:02 +0545

    Removing unnecessary comment

--- models/Admin.js
+++ models/Admin.js
@@ -1,34 +1,3 @@
-/*
-import mongoose from "mongoose";
-
-const adminSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 6,
-  },
-  addedMovies: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: "Movie",
-    },
-  ],
-  otp: {
-    type: String, // OTP that will be sent to the admin's email
-  },
-  otpExpiration: {
-    type: Number, // Timestamp for OTP expiration
-  },
-});
-
-export default mongoose.model("Admin", adminSchema);
-*/
-
 
 import mongoose from "mongoose";
 
--- models/Movie.js
+++ models/Movie.js
@@ -1,5 +1,4 @@
 
-
 import mongoose from "mongoose";
 
 const movieSchema = new mongoose.Schema({
